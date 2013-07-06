f1b2e993c6c3c44aa49b11b25efe6fafe4006078
alznlv <alznlv@gmail.com>
2013-07-06 15:19:01 +0800

    test3

--- dmengine/TMCLI/4.py
+++ dmengine/TMCLI/4.py
@@ -1,4 +1,4 @@
-1import subprocess
+import subprocess
 
 def getoutput():
     change_file_list=[]
@@ -7,12 +7,15 @@ def getoutput():
         out=''
         out=p.stdout.readline().strip('\n')
         print out
-        if len(out.split('|'))>1:
-            change_file_list.append(out.split("|")[0])
+        if out.find('Already up-to-date.')>=0:
+            break;
+        outlist=out.split('|')
+        if len(outlist)>1 and outlist[0].find('dmengine/')>=0:
+            change_file_list.append(outlist[0].strip())
             print change_file_list
-        else:
-            print len(out.split('|'))
-            print out.split('|')
+#         else:
+#             print len(out.split('|'))
+#             print out.split('|')
         
     print p.returncode
 
