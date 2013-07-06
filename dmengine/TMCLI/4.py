1cb24a35bf02c083e74d5920f3d2116e54df1acb
alznlv <alznlv@gmail.com>
2013-07-06 11:48:43 +0800

    test3

--- dmengine/TMCLI/4.py
+++ dmengine/TMCLI/4.py
@@ -1,1 +1,19 @@
-1
+1import subprocess
+
+def getoutput():
+    change_file_list=[]
+    p = subprocess.Popen('git pull',stdout=subprocess.PIPE,shell=True)
+    while p.poll() is None:
+        out=''
+        out=p.stdout.readline().strip('\n')
+        print out
+        if len(out.split('|'))>1:
+            change_file_list.append(out.split("|")[0])
+            print change_file_list
+        else:
+            print len(out.split('|'))
+            print out.split('|')
+        
+    print p.returncode
+
+getoutput()
