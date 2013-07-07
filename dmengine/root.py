c9d7e4c8ad19068469de21dd41c178fb7dc81943
alznlv <alznlv@gmail.com>
2013-07-07 22:35:56 +0800

    test create and delete

--- dmengine/root.py
+++ dmengine/root.py
@@ -9,10 +9,18 @@ def getoutput():
         print out
         if out.find('Already up-to-date.')>=0:
             break;
+        if out.find('create mode')>=0:
+            print 'file be created'
+            print out.split(' ')[-1]
+        if out.find('delete mode')>=0:
+            print 'file be deleted'
+            print out.split(' ')[-1]
+        if out.find('rename')>=0:
+            print 'file be renamed'
         outlist=out.split('|')
         if len(outlist)>1 and outlist[0].find('dmengine/')>=0:
             change_file_list.append(outlist[0].strip())
-            print change_file_list
+            print 'update file list:'change_file_list
     if p.poll() is None:
         p.kill()    
     print p.returncode
